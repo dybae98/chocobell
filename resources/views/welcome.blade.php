3763cbddd05623256910d6806386512b8fcbe2ab
Mariana Gallegos <xmarianagallegos@gmail.com>
2021-07-14 21:07:33 -0500

    Cambios manchas

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    LaravelManchas
                 </div>
 
                 <div class="links">
