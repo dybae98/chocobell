a3c0234fc27a2bedcac663fd5cec88874e9b14f9
Mariana Gallegos <xmarianagallegos@gmail.com>
2021-07-14 21:13:59 -0500

    intento2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    LaravelManchas2
+                    LaravelManchas22
                 </div>
 
                 <div class="links">
