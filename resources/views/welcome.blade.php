49df3414e692b7fb9aae884970b8fc4f788e6d33
Mariana Gallegos <xmarianagallegos@gmail.com>
2021-07-14 21:43:38 -0500

    cheetos

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    LaravelManchas2
+                    m kiero morir owo
                 </div>
 
                 <div class="links">
